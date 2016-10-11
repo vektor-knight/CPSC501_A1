fe486603a3062af77e2232225a1148182747155a
vektor <vektor@vektor-PC>
2016-10-10 19:14:10 -0600

    "Person" class created; no refactorings.

---
+++ src/cpsc501_a1/CPSC501_A1.java
@@ -0,0 +1,30 @@
+package cpsc501_a1;
+
+/**
+ *
+ * @author Sartaj Hundal
+ */
+class Person {
+  private String name;
+  private String officeAreaCode;
+  private String officeNumber;
+
+  public String getName() {
+    return name;
+  }
+  public String getTelephoneNumber() {
+    return ("(" + officeAreaCode + ") " + officeNumber);
+  }
+  public String getOfficeAreaCode() {
+    return officeAreaCode;
+  }
+  public void setOfficeAreaCode(String arg) {
+    officeAreaCode = arg;
+  }
+  public String getOfficeNumber() {
+    return officeNumber;
+  }
+  public void setOfficeNumber(String arg) {
+    officeNumber = arg;
+  }
+}
