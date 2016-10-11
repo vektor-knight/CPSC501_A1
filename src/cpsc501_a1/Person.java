016c9e9bdd53e8b054bfab52ca27ef54e39f3619
vektor <vektor@vektor-PC>
2016-10-10 19:44:43 -0600

    Refactor 3: Introduce Foreign Method to delegate data from TelephoneNum

--- src/cpsc501_a1/Person.java
+++ src/cpsc501_a1/Person.java
@@ -6,23 +6,32 @@ package cpsc501_a1;
  */
 class Person {
   private String name;
+  // Now, we must introduce a foreign method from
+  // the newly created TelephoneNumber class.
+  // Then, the attributes of Person that are used
+  // in the getters and setters here will be accessing
+  // data from the TelephoneNumber class instead.
+  private final TelephoneNumber officePhone = new TelephoneNumber();
   
   public String getName() {
     return name;
   }
   public String getTelephoneNumber() {
-    return ("(" + officeAreaCode + ") " + officeNumber);
+   return officePhone.getAreaCode();
   }
   public String getOfficeAreaCode() {
-    return officeAreaCode;
+   // Instead of returning data local to Person's scope,
+   // we will now query TelephoneNumber instead.
+   // return ("(" + officeAreaCode + ") " + officeNumber);
+   return officePhone.getAreaCode();
   }
   public void setOfficeAreaCode(String arg) {
-    officeAreaCode = arg;
+    officePhone.setAreaCode(arg);
   }
   public String getOfficeNumber() {
-    return officeNumber;
+    return officePhone.getAreaCode();
   }
   public void setOfficeNumber(String arg) {
-    officeNumber = arg;
+    officePhone.setNumber(arg);
   }
 }
